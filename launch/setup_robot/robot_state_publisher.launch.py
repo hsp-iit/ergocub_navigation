479117aa6b48ed6b300e4e347dd7f1bcae604e68
ergocub <ergocub@iit.it>
2023-07-06 20:31:21 +0200

    updated urdf model path for ergoCubSN000

--- launch/setup_robot/robot_state_publisher.launch.py
+++ launch/setup_robot/robot_state_publisher.launch.py
@@ -9,7 +9,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     #use_sim_time = LaunchConfiguration('use_sim_time', default=True)
 
-    robot_urdf = '/home/user1/robotology-superbuild/build/install/share/ergoCub/robots/ergoCubGazeboV1/model.urdf'
+    robot_urdf = '/usr/local/src/robot/navigation/ergocub-software/urdf/ergoCub/robots/ergoCubSN000/model.urdf'
     with open(robot_urdf, 'r') as infp:
         robot_desc = infp.read()
 
@@ -19,7 +19,7 @@ def generate_launch_description():
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description': robot_desc , 'publish_frequency': 200.0, 'use_sim_time': True}],
+            parameters=[{'robot_description': robot_desc , 'publish_frequency': 200.0, 'use_sim_time': False}],
             arguments=[robot_urdf])
     ])
 
