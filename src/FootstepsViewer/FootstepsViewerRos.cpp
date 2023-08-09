45ec11a7a450ad2846be8fb4f3e7438c4f9129f8
ergocub <ergocub@iit.it>
2023-08-09 17:14:06 +0200

    separated in multiple files scan and footsteps_viewer

--- src/FootstepsViewer/FootstepsViewerRos.cpp
+++ src/FootstepsViewer/FootstepsViewerRos.cpp
@@ -1,45 +1,12 @@
-#include "yarp/os/Bottle.h"
-#include "yarp/os/BufferedPort.h"
-#include "yarp/os/Network.h"
-#include "yarp/sig/Vector.h"
-#include "yarp/os/ConnectionWriter.h"
-#include "yarp/os/Portable.h"
+#include "FootstepsViewer/FootstepsViewerRos.hpp"
 
-#include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/path.hpp"
-#include "nav_2d_msgs/msg/pose2_d_stamped.hpp"
-#include "nav_2d_msgs/msg/path2_d.hpp"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "visualization_msgs/msg/marker_array.hpp"
+FootstepsViewerRos::FootstepsViewerRos() : rclcpp::Node("footstep_viewer_node")
+{   
+    m_rightFootprintsMarkersPub = this->create_publisher<visualization_msgs::msg::MarkerArray>(m_rightFootprintsTopicName, 10);
+    m_leftFootprintsMarkersPub = this->create_publisher<visualization_msgs::msg::MarkerArray>(m_leftFootprintsTopicName, 10);
+};
 
-#include <mutex>
-#include <thread>
-#include <chrono>
-
-using namespace std::chrono_literals;
-using std::placeholders::_1;
-
-//This class has the purpose to publish data on ros topics
-class FootstepsViewerRos : public rclcpp::Node
-{
-private:
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr m_rightFootprintsMarkersPub;
-    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr m_leftFootprintsMarkersPub;
-    const std::string m_rightFootprintsTopicName = "/footstep_viewer_node/right_footprints";
-    const std::string m_leftFootprintsTopicName = "/footstep_viewer_node/left_footprints";
-
-    std::mutex m_rosMutex;
-    
-public:
-    FootstepsViewerRos() : rclcpp::Node("footstep_viewer_node")
-    {   
-        m_rightFootprintsMarkersPub = this->create_publisher<visualization_msgs::msg::MarkerArray>(m_rightFootprintsTopicName, 10);
-        m_leftFootprintsMarkersPub = this->create_publisher<visualization_msgs::msg::MarkerArray>(m_leftFootprintsTopicName, 10);
-    }
-
-    bool publishMarkers(const yarp::os::Bottle& data){
+bool FootstepsViewerRos::publishMarkers(const yarp::os::Bottle& data){
         std::cout << "publishMarkers" << std::endl;
         std::cout << data.size() << std::endl;
         auto leftSteps = data.get(0).asList();
@@ -158,87 +125,4 @@ public:
         //publish
         m_rightFootprintsMarkersPub->publish(right_marker_array);
         std::cout << "Exiting" << std::endl;
-
-
-    }
-};  //End of class FootstepsViewerRos
-
-//Class used for YARP port callbacks. Monitors the feet contacts state.
-class FootstepsViewerYarp : public yarp::os::PortReader
-{
-private:
-    std::mutex m_mutex;
-    std::shared_ptr<FootstepsViewerRos> m_rosNode;
-    bool m_initialized = false;
-
-public:
-
-    FootstepsViewerYarp()
-    {
-    };
-    //main loop executed for each port reading of the merged feet status
-    bool read(yarp::os::ConnectionReader& t_connection) override
-    {
-        std::lock_guard<std::mutex> guard(m_mutex);
-        if (!m_initialized)
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        }
-        std::cout << "New message received" << std::endl;
-        yarp::os::Bottle b;
-        bool ok = b.read(t_connection);
-        if (!ok) {
-            std::cout << "No connection available for reading data " << std::endl;
-            return false;
-        }
-        if (m_initialized)
-        {
-            try
-            {
-                m_rosNode->publishMarkers(b);
-            }
-            catch(const std::exception& e)
-            {
-                std::cerr << e.what() << '\n';
-            }
-        }
-        else
-        {
-            std::cout << "ROS node not yet initialized" << std::endl;
-        }
-        return true;
-    }
-
-    void runROS()
-    {
-        m_rosNode = std::make_shared<FootstepsViewerRos>();
-        m_initialized = true;
-        rclcpp::spin(m_rosNode);
-    }
-
-
-    
-};  //End of class FootstepsViewerYarp : public yarp::os::PortReader
-
-int main(int argc, char** argv)
-{
-    const std::string footprintsPortName = "/footsteps_viewer/footsteps:i";
-    // Init ROS2
-    rclcpp::init(argc, argv);
-    //Init YARP
-    yarp::os::Network yarp;
-    // Start listening in polling
-    if (rclcpp::ok())
-    {
-        FootstepsViewerYarp dataProcessor;
-        yarp::os::Port footprintsPort;
-        footprintsPort.open(footprintsPortName);
-        yarp::os::Network::connect("/navigation_helper/feet_positions:o", footprintsPortName);
-        footprintsPort.setReader(dataProcessor);
-        dataProcessor.runROS();
-        //rclcpp::spin(node);
-    }
-    std::cout << "Shutting down" << std::endl;
-    rclcpp::shutdown();
-    return 0;
-}
+};
---
+++ src/FootstepsViewer/FootstepsViewerYarp.cpp
@@ -0,0 +1,42 @@
+#include "FootstepsViewer/FootstepsViewerYarp.hpp"
+
+FootstepsViewerYarp::FootstepsViewerYarp(){};
+
+bool FootstepsViewerYarp::read(yarp::os::ConnectionReader& t_connection)
+{
+    std::lock_guard<std::mutex> guard(m_mutex);
+    if (!m_initialized)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+    std::cout << "New message received" << std::endl;
+    yarp::os::Bottle b;
+    bool ok = b.read(t_connection);
+    if (!ok) {
+        std::cout << "No connection available for reading data " << std::endl;
+        return false;
+    }
+    if (m_initialized)
+    {
+        try
+        {
+            m_rosNode->publishMarkers(b);
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    else
+    {
+        std::cout << "ROS node not yet initialized" << std::endl;
+    }
+    return true;
+}
+
+void FootstepsViewerYarp::runROS()
+{
+    m_rosNode = std::make_shared<FootstepsViewerRos>();
+    m_initialized = true;
+    rclcpp::spin(m_rosNode);
+}
