8ad5d3d2e92a92ac8dbada60073aa2b05c70fd3d
ergocub <ergocub@iit.it>
2023-08-21 16:17:18 +0200

    added launch and config files for imu_tools pkg

---
+++ launch/imu/imu_complementary_filter.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    parms_path = os.path.join(get_package_share_directory('ergocub_navigation'), 'param', "imu_filter.yaml")
+    return LaunchDescription(
+        [
+            Node(
+                package='imu_complementary_filter',
+                remappings=[('imu/data_raw', 'head_imu/data_raw')],
+                executable='complementary_filter_node',
+                name='complementary_filter_gain_node',
+                output='screen',
+                parameters=[parms_path],
+            )
+        ]
+    )
---
+++ launch/imu/imu_madgwick_filter.launch.py
@@ -0,0 +1,24 @@
+import os
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    parms_path = os.path.join(get_package_share_directory('ergocub_navigation'), 'param', "imu_filter.yaml")
+
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package='imu_filter_madgwick',
+                remappings=[('imu/data_raw', 'head_imu/data_raw')],
+                executable='imu_filter_madgwick_node',
+                name='imu_filter',
+                output='screen',
+                parameters=[parms_path]
+            )
+        ]
+    )
