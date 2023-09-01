b8b87ffd5966ceaa68618f1108de4afcc57496cc
ergocub <ergocub@iit.it>
2023-09-01 15:10:17 +0200

    reduced imu threshold and removed unused tf dep

--- include/PointcloudFilter/PointcloudFilter.hpp
+++ include/PointcloudFilter/PointcloudFilter.hpp
@@ -1,17 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "sensor_msgs/msg/imu.hpp"
-#include "tf2_ros/transform_listener.h"
-#include "tf2_ros/buffer.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include "tf2/transform_datatypes.h"
-#include "tf2_sensor_msgs/tf2_sensor_msgs.hpp"
-
-#include "pcl_conversions/pcl_conversions.h"
-#include "pcl/point_types.h"
-#include "pcl/filters/passthrough.h"
-#include "pcl/ModelCoefficients.h"
-#include "pcl/filters/project_inliers.h"
 
 #include <mutex>
 
@@ -21,17 +10,16 @@ private:
     const std::string m_referece_frame = "geometric_unicycle";  //virtual_unicycle_base
     const std::string m_depth_topic = "/camera/depth/color/points";
     const std::string m_pub_topic = "/imu_filtered_depth";
+    const std::string m_pub_unfiltered_topic = "/imu_unfiltered_depth";
     const std::string m_imu_topic = "/head_imu";
 
-    const double m_close_threshold = 0.5;
-    const double m_imuVel_x_threshold = 0.4;
-    const double m_imuVel_y_threshold = 0.4;
-    const double m_ms_wait = 400.0;
+    const double m_imuVel_x_threshold = 0.3;
+    const double m_imuVel_y_threshold = 0.3;
+    const double m_ms_wait = 500.0;
     std::chrono::system_clock::time_point m_last_vibration_detection;
 
-    std::shared_ptr<tf2_ros::TransformListener> m_tf_listener_{nullptr};
-    std::unique_ptr<tf2_ros::Buffer> m_tf_buffer_in;
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_filtered_pointcloud_pub;
+    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_unfiltered_pub;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr m_raw_depth_sub;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr m_imu_sub;
     geometry_msgs::msg::Vector3 m_imu_angular_velocity;
--- src/PointcloudFilter.cpp
+++ src/PointcloudFilter.cpp
@@ -1,5 +1,10 @@
 #include "PointcloudFilter/PointcloudFilter.hpp"
 
+#include "pcl_conversions/pcl_conversions.h"
+#include "pcl/point_types.h"
+#include "pcl/filters/statistical_outlier_removal.h"
+#include "pcl/ModelCoefficients.h"
+
 #include <cmath>
 #include <chrono>
 
@@ -29,22 +34,38 @@ void PointcloudFilter::depth_callback(const sensor_msgs::msg::PointCloud2::Const
                     return;
                 }
             }
-            //Echo the message
+            /* //TOO SLOW
+            pcl::PointCloud<pcl::PointXYZ>::Ptr in_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::fromROSMsg(*pc_in, *in_cloud);
+
+            // Create the filtering object
+            pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
+            sor.setInputCloud (in_cloud);
+            sor.setMeanK (50);
+            sor.setStddevMulThresh (1.0);
+            sor.filter (*cloud_filtered);
             sensor_msgs::msg::PointCloud2 pc_out;
-            pc_out.header = pc_in->header;
-            pc_out.data = pc_in->data;
-            pc_out.fields = pc_in->fields;
-            pc_out.height = pc_in->height;
-            pc_out.is_bigendian = pc_in->is_bigendian;
-            pc_out.is_dense = pc_in->is_dense;
-            pc_out.point_step = pc_in->point_step;
-            pc_out.row_step = pc_in->row_step;
-            pc_out.width = pc_in->width;
+            pcl::toROSMsg(*cloud_filtered, pc_out);
             m_filtered_pointcloud_pub->publish(pc_out);
+            */
+ 
+            //Echo the message
+            sensor_msgs::msg::PointCloud2 pc_out_unfiltered;
+            pc_out_unfiltered.header = pc_in->header;
+            pc_out_unfiltered.data = pc_in->data;
+            pc_out_unfiltered.fields = pc_in->fields;
+            pc_out_unfiltered.height = pc_in->height;
+            pc_out_unfiltered.is_bigendian = pc_in->is_bigendian;
+            pc_out_unfiltered.is_dense = pc_in->is_dense;
+            pc_out_unfiltered.point_step = pc_in->point_step;
+            pc_out_unfiltered.row_step = pc_in->row_step;
+            pc_out_unfiltered.width = pc_in->width;
+            m_unfiltered_pub->publish(pc_out_unfiltered);
         }
         catch(const std::exception& e)
         {
-            std::cerr << e.what() << '\n';
+            RCLCPP_ERROR(this->get_logger(), e.what());
         }
     };
 
@@ -57,15 +78,14 @@ void PointcloudFilter::imu_callback(const sensor_msgs::msg::Imu::ConstPtr& imu_m
     }
     catch(const std::exception& e)
     {
-        std::cerr << e.what() << '\n';
+        RCLCPP_ERROR(this->get_logger(), e.what());
     }
 };
 
 PointcloudFilter::PointcloudFilter() : Node("depth_filtering_node")
 {
     m_filtered_pointcloud_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(m_pub_topic, 10);
-    m_tf_buffer_in = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-    m_tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*m_tf_buffer_in);
+    m_unfiltered_pub = this->create_publisher<sensor_msgs::msg::PointCloud2>(m_pub_unfiltered_topic , 10);
     m_last_vibration_detection = std::chrono::system_clock::now();
 
     m_raw_depth_sub = this->create_subscription<sensor_msgs::msg::PointCloud2> (
