68ef4e3b055fc8887b13054e157fa84bd88d3343
Simone <simone_micheletti@outlook.it>
2023-10-31 17:58:06 +0000

    First Release of managed lifecycle node

--- include/ScanFilter/ScanFilter.hpp
+++ include/ScanFilter/ScanFilter.hpp
@@ -8,6 +8,9 @@
 #include "tf2_sensor_msgs/tf2_sensor_msgs.hpp"
 #include "laser_geometry/laser_geometry.hpp"
 
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/lifecycle_publisher.hpp"
+
 #include "pcl_conversions/pcl_conversions.h"
 #include "pcl/point_types.h"
 #include "pcl/filters/passthrough.h"
@@ -16,32 +19,30 @@
 
 #include <mutex>
 
-class ScanFilter : public rclcpp::Node
+class ScanFilter : public rclcpp_lifecycle::LifecycleNode
 {
 private:
-    const std::string m_referece_frame = "geometric_unicycle";  //virtual_unicycle_base
-    const char* m_scan_topic = "/scan_local";
-    const std::string m_pub_topic = "/compensated_pc2";
-    const std::string m_imu_topic = "/head_imu";
+    //parameters
+    std::string m_referece_frame = "geometric_unicycle";  //virtual_unicycle_base
+    std::string m_scan_topic = "/scan_local";
+    std::string m_pub_topic = "/compensated_pc2";
+    std::string m_imu_topic = "/head_imu";
     laser_geometry::LaserProjection m_projector;
 
-    const float m_filter_z_low = 0.2;
-    const float m_filter_z_high = 2.5;
-    const float m_sensor_height = 1.5;
-    const double m_close_threshold = 0.5;
-    const double m_imuVel_x_threshold = 0.4;
-    const double m_imuVel_y_threshold = 0.4;
-    const double m_ms_wait = 400.0;
+    float m_filter_z_low = 0.2;
+    float m_filter_z_high = 2.5;
+    double m_close_threshold = 0.5;
+    double m_imuVel_x_threshold = 0.4;
+    double m_imuVel_y_threshold = 0.4;
+    double m_ms_wait = 400.0;
     std::chrono::system_clock::time_point m_last_vibration_detection;
-
-    const char* m_right_sole_frame = "r_sole";
-    const char* m_left_sole_frame = "l_sole";
 
     bool m_robot_on_crane = true;
 
     std::shared_ptr<tf2_ros::TransformListener> m_tf_listener_{nullptr};
     std::unique_ptr<tf2_ros::Buffer> m_tf_buffer_in;
-    rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_pointcloud_pub, m_second_pub, m_third_pub;
+
+    rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::PointCloud2>::SharedPtr m_pointcloud_pub;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr m_raw_scan_sub;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr m_imu_sub;
     geometry_msgs::msg::Vector3 m_imu_angular_velocity;
@@ -52,5 +53,14 @@ private:
     void imuCallback(const sensor_msgs::msg::Imu::ConstPtr& imu_msg);
 
 public:
-    ScanFilter();
+    ScanFilter(const rclcpp::NodeOptions & options);
+
+    using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &);
+    CallbackReturn on_activate(const rclcpp_lifecycle::State &);
+    CallbackReturn on_deactivate(const rclcpp_lifecycle::State &);
+    CallbackReturn on_cleanup(const rclcpp_lifecycle::State &);
+    CallbackReturn on_shutdown(const rclcpp_lifecycle::State & state);
+    CallbackReturn on_error(const rclcpp_lifecycle::State & state);
 };  // End of class ScanFilter node
