9462d13ace6247dcbe6110550e1b6128eeaa97f9
vigisushrutha23 <vigisushrutha23@gmail.com>
2023-10-31 15:27:40 +0000

    added launch files for simulation

---
+++ launch/setup_robot/robot_state_publisher_simulation.launch.py
@@ -0,0 +1,25 @@
+from ast import arg
+import imp
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    #use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    robot_urdf = '/home/user1/robotology-superbuild/build/install/share/ergoCub/robots/ergoCubGazeboV1/model.urdf'
+    with open(robot_urdf, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+            
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc , 'publish_frequency': 200.0, 'use_sim_time': False}],
+            arguments=[robot_urdf])
+    ])
+
---
+++ launch/setup_robot_simulation.launch.py
@@ -0,0 +1,65 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    state_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/setup_robot/robot_state_publisher_simulation.launch.py'])
+        )
+    rviz_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/ergoCub_rviz.launch.py'])
+        )
+    projection_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/setup_robot/chest_projection.launch.py'])
+        )
+    scan_filtering_compensated = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/setup_robot/scan_filtering_compensated.launch.py'])
+        )
+    depth_to_pointcloud = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/setup_robot/point_cloud_xyz.launch.py'])
+        )
+    virtual_unicycle = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/setup_robot/odom.launch.py'])
+        )
+    amcl = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/setup_localization.launch.py'])
+        )
+    footprints_viewer = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            '/footprints_viewer.launch.py'])
+        )
+    return LaunchDescription([
+        state_publisher,
+        projection_node,
+        #rviz_node,
+        scan_filtering_compensated,
+        #depth_to_pointcloud,
+        virtual_unicycle,
+        amcl,
+        #footprints_viewer,
+        Node(
+            package='ergocub_navigation',
+            executable='pointcloud_filter',
+            output='screen',
+        )
+    ])
