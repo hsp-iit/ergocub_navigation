acfe31c278235ca358464d08827de4c114768e4e
SimoneMic <simone_micheletti@outlook.it>
2023-12-01 14:37:46 +0000

    setup robot files for simulation only

---
+++ launch/simulation/amcl/amcl_sim.launch.py
@@ -0,0 +1,46 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+package_name = 'ergocub_navigation'
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    
+    param_file_name = 'ergoCub_amcl_2' + '.yaml'
+
+    param_dir=os.path.join(
+            get_package_share_directory(package_name),
+            'param',
+            param_file_name)
+      
+    amcl_params = os.path.join(os.getcwd(), param_dir)
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='Use simulation (Gazebo) clock if true'),
+
+        Node(
+            package='nav2_amcl',
+            executable='amcl',
+            name='amcl',
+            parameters=[amcl_params]
+        ),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': True},
+                        {'node_names': ['amcl']}]
+        )
+    ])
---
+++ launch/simulation/setup_robot/chest_projection_sim.launch.py
@@ -0,0 +1,30 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    
+    config = os.path.join(
+        get_package_share_directory('ergocub_navigation'),
+        'config/param',
+        'chest_projection_params.yaml'
+        )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+        Node(
+            package='ergocub_navigation',
+            executable='base_projector',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}]
+        )
+    ])
