a5e71cabffbbf3c2aa5415163fa0d3afb311da0d
ergocub <ergocub@iit.it>
2024-09-11 11:01:57 +0200

    WIP with ergocubSn002

--- include/LocalHumanAvoidance/LocalHumanAvoidance.hpp
+++ include/LocalHumanAvoidance/LocalHumanAvoidance.hpp
@@ -26,21 +26,25 @@
 #include "visualization_msgs/msg/marker_array.hpp"
 #include <tf2/exceptions.h>
 #include <tf2_ros/transform_listener.h>
+#include "tf2_ros/transform_broadcaster.h"
 #include <tf2_ros/buffer.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include <yarp/os/Network.h>
 #include <yarp/os/Port.h>
 #include <yarp/os/BufferedPort.h>
 #include <yarp/os/Bottle.h>
 #include <yarp/os/RpcClient.h>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
 #include "ControlInterface.h"
 #include "eCubPerceptionInterface/eCubPerceptionInterface.h"
 
-
 namespace ergocub_local_human_avoidance
 {
-    class HumanAvoidanceController : public nav2_core::Controller //class declaration based on nav2_tutorials custom controller plugin
+    class HumanAvoidanceController : public nav2_core::Controller // class declaration based on nav2_tutorials custom controller plugin
     {
     public:
         /**
@@ -66,7 +70,7 @@ namespace ergocub_local_human_avoidance
         void setSpeedLimit(const double &speed_limit, const bool &percentage) override;
 
         /**
-         * Plugin functions that is called to give velocity commands but in this case used to also determine object pose changes and nav shifts. 
+         * Plugin functions that is called to give velocity commands but in this case used to also determine object pose changes and nav shifts.
          **/
 
         geometry_msgs::msg::TwistStamped computeVelocityCommands(const geometry_msgs::msg::PoseStamped &pose,
@@ -76,7 +80,7 @@ namespace ergocub_local_human_avoidance
         /**
          * Function to set global plan to the local variable
          **/
-        void setPlan(const nav_msgs::msg::Path &path) override; 
+        void setPlan(const nav_msgs::msg::Path &path) override;
 
     protected:
         // nav_msgs::msg::Path transformGlobalPlan(const geometry_msgs::msg::PoseStamped &pose);
@@ -91,49 +95,47 @@ namespace ergocub_local_human_avoidance
         rclcpp_lifecycle::LifecycleNode::WeakPtr node_;
         std::shared_ptr<tf2_ros::Buffer> tf_;
         std::string plugin_name_;
-        std::string human_left_frame_; //Name of frame attached on the left extreme of the detected human
-        std::string human_right_frame_; //Name of frame attached on the right extreme of the detected human
-        std::string human_tf_base_frame_; //Name of frame attached on the pelvis of the detected human
+        std::string human_left_frame_;                               // Name of frame attached on the left extreme of the detected human
+        std::string human_right_frame_;                              // Name of frame attached on the right extreme of the detected human
+        std::string human_tf_base_frame_;                            // Name of frame attached on the pelvis of the detected human
         std::shared_ptr<nav2_costmap_2d::Costmap2DROS> costmap_ros_; // Variable to store local command
         rclcpp::Logger logger_{rclcpp::get_logger("HumanAvoidanceController")};
         rclcpp::Clock::SharedPtr clock_;
-        std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_; //publisher to publish path
+        std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<nav_msgs::msg::Path>> global_pub_; // publisher to publish path
+        std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
-        double desired_linear_vel_; //velocity commands.
-        double lookahead_dist_; // Distance to look ahead of current pose.
-        double max_angular_vel_; //max allowed rotation speed.
-        double object_size_; //size of the object held by the robot, currently unused.
-        double safe_dist_to_human_; //Safe distance required between the robot and detected human.
-        double current_human_horizontal_dist_; //Variable to store the current horizontal (y-axis) distance of the human extreme w.r.t robot.
-        double executed_translation_; //Variable to record the last executed object translation.
-        double executed_rotation_; //Variable to record the last executed object rotation.
-        double human_dist_threshold_; // Threshold at which the robot should start making changes to the object pose if the human is close.
-        double horizontal_dist_modifier_; // Typically a multiplier to horizontal dist from camera to account for inaccuracies.
-        bool nav_shift_enabled_; //Bool to inform if nav shift is enabled.
-        bool obj_pose_action_executed_; //Bool to check if object pose change has already started.
-        bool reset_executed_; //Bool to check if the object pose was reset recently.
+        double desired_linear_vel_;                  // velocity commands.
+        double lookahead_dist_;                      // Distance to look ahead of current pose.
+        double max_angular_vel_;                     // max allowed rotation speed.
+        double object_size_;                         // size of the object held by the robot, currently unused.
+        double safe_dist_to_human_;                  // Safe distance required between the robot and detected human.
+        double current_human_horizontal_dist_;       // Variable to store the current horizontal (y-axis) distance of the human extreme w.r.t robot.
+        double executed_translation_;                // Variable to record the last executed object translation.
+        double executed_rotation_;                   // Variable to record the last executed object rotation.
+        double human_dist_threshold_;                // Threshold at which the robot should start making changes to the object pose if the human is close.
+        double horizontal_dist_modifier_;            // Typically a multiplier to horizontal dist from camera to account for inaccuracies.
+        bool nav_shift_enabled_;                     // Bool to inform if nav shift is enabled.
+        bool obj_pose_action_executed_;              // Bool to check if object pose change has already started.
+        bool reset_executed_;                        // Bool to check if the object pose was reset recently.
         rclcpp::Duration transform_tolerance_{0, 0}; // Transform duration difference tolerence, currently unused.
 
         nav_msgs::msg::Path global_plan_;
 
-        double obj_max_translation_; // Param to store max allowed object pose tranlsation.
-        double obj_max_rotation_; //Param to store max allowed object pose rotation.
-        double obj_translation_slope_; //Param used to determine translation of object as (obj_translation_slope x required separation from human) 
-        double obj_orientation_slope_; //Param used to determine rotation of object as (obj_orientation_slope x required separation from human)
+        double obj_max_translation_;   // Param to store max allowed object pose tranlsation.
+        double obj_max_rotation_;      // Param to store max allowed object pose rotation.
+        double obj_translation_slope_; // Param used to determine translation of object as (obj_translation_slope x required separation from human)
+        double obj_orientation_slope_; // Param used to determine rotation of object as (obj_orientation_slope x required separation from human)
 
         // yarp stuff
-        yarp::os::Network yarp_; // Yarp network declaration to allow for sending messages to navigation and bimanual ports.
+        yarp::os::Network yarp_;        // Yarp network declaration to allow for sending messages to navigation and bimanual ports.
         yarp::os::Port bimannual_port_; // Port to the Bimanual Server to allow for held object pose change
-        yarp::os::RpcClient human_extremes_port_; 
-        yarp::os::BufferedPort<yarp::os::Bottle> nav_shift_port_; //Port to path converter to shift planned path
+        yarp::os::RpcClient human_extremes_port_;
+        yarp::os::BufferedPort<yarp::os::Bottle> nav_shift_port_;         // Port to path converter to shift planned path
         yarp::os::BufferedPort<yarp::os::Bottle> direct_human_data_port_; // Port to directly read human data
-        ControlInterface bimanual_client_; //Thrift interface for peforming grasp actions that cause object pose change
+        ControlInterface bimanual_client_;                                // Thrift interface for peforming grasp actions that cause object pose change
         eCubPerceptionInterface human_data_client_;
-        std::string nav_shift_port_name_; //Name of the port to send path shift command
-        std::string bimanual_server_name_; //Name of the bimanual server.
-
-
-        
+        std::string nav_shift_port_name_;  // Name of the port to send path shift command
+        std::string bimanual_server_name_; // Name of the bimanual server.
     };
 
 } // end of namespace
--- src/LocalHumanAvoidance.cpp
+++ src/LocalHumanAvoidance.cpp
@@ -113,6 +113,8 @@ namespace ergocub_local_human_avoidance
     }
     else
       nav_shift_enabled_ = true;
+
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(node);
   }
 
   void HumanAvoidanceController::cleanup()
@@ -183,20 +185,6 @@ namespace ergocub_local_human_avoidance
       return cmd_vel;
     }
 
-    geometry_msgs::msg::TransformStamped right_transform;
-    try
-    {
-      right_transform = tf_->lookupTransform(
-          human_tf_base_frame_, human_right_frame_,
-          tf2::TimePointZero);
-    }
-    catch (const tf2::TransformException &ex)
-    {
-      RCLCPP_INFO(
-          logger_, "Could not transform %s to %s: %s",
-          human_tf_base_frame_.c_str(), human_right_frame_.c_str(), ex.what());
-      return cmd_vel;
-    }
 
     RCLCPP_INFO(
         logger_,
@@ -214,6 +202,20 @@ namespace ergocub_local_human_avoidance
       RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_three_ints");
       return cmd_vel;
     }*/
+    geometry_msgs::msg::TransformStamped robot_transform;
+    try
+    {
+      robot_transform = tf_->lookupTransform(
+          "geometric_unicycle", "realsense",
+          tf2::TimePointZero);
+    }
+    catch (const tf2::TransformException &ex)
+    {
+      RCLCPP_INFO(
+          logger_, "Could not transform %s to %s: %s",
+          human_tf_base_frame_.c_str(), human_right_frame_.c_str(), ex.what());
+      return cmd_vel;
+    }
 
     RCLCPP_INFO(
         logger_,
@@ -226,13 +228,55 @@ namespace ergocub_local_human_avoidance
         logger_,
         "\n======================== Got human values %f==========================\n", human_data->get(2).asFloat64());
 
-    if (human_data != nullptr && human_data->get(2).asFloat64() > 0.0)
+    if (human_data != nullptr && human_data->get(2).asFloat64() > 0.0 && human_data->get(2).asFloat64() < 5.0)
     {
+
+      Eigen::MatrixXd robot_from_base = Eigen::MatrixXd::Identity(4, 4);
+      robot_from_base.block(0, 0, 3, 3) = Eigen::Quaterniond(robot_transform.transform.rotation.w, robot_transform.transform.rotation.x, robot_transform.transform.rotation.y, robot_transform.transform.rotation.z).toRotationMatrix();
+      Eigen::Vector3d robot_trans_from_odom;
+      robot_trans_from_odom << robot_transform.transform.translation.x, robot_transform.transform.translation.y, robot_transform.transform.translation.z;
+      robot_from_base.block(0, 3, 3, 1) = robot_trans_from_odom;
+      Eigen::MatrixXd human_left_from_camera = Eigen::MatrixXd::Identity(4, 4);
+      Eigen::MatrixXd human_right_from_camera = Eigen::MatrixXd::Identity(4, 4);
+
+      Eigen::Vector3d human_left_trans_vector;
+      human_left_trans_vector << human_data->get(2).asFloat64(), -human_data->get(0).asFloat64() - human_data->get(3).asFloat64(), 0.0;
+      Eigen::Vector3d human_right_trans_vector;
+      human_right_trans_vector << human_data->get(2).asFloat64(), -human_data->get(0).asFloat64() - human_data->get(4).asFloat64(), 0.0;
+      human_left_from_camera.block(0, 3, 3, 1) = human_left_trans_vector;
+      human_right_from_camera.block(0, 3, 3, 1) = human_right_trans_vector;
+
+      Eigen::Matrix4d human_left_from_robot = robot_from_base * human_left_from_camera;
+      Eigen::Matrix4d human_right_from_robot = robot_from_base * human_right_from_camera;
+      Eigen::Matrix3d human_left_from_robot_rot = human_left_from_robot.block(0, 0, 3, 3);
+      Eigen::Matrix3d human_right_from_robot_rot = human_right_from_robot.block(0, 0, 3, 3);
+
+      Eigen::Quaterniond human_left_from_robot_quat(human_left_from_robot_rot);
+      Eigen::Quaterniond human_right_from_robot_quat(human_right_from_robot_rot);
+
       geometry_msgs::msg::TransformStamped left_transform, right_transform;
-      left_transform.transform.translation.x = human_data->get(2).asFloat64();
-      left_transform.transform.translation.y = -human_data->get(0).asFloat64() - human_data->get(3).asFloat64();
-      right_transform.transform.translation.x = human_data->get(2).asFloat64();
-      right_transform.transform.translation.y = -human_data->get(0).asFloat64() - human_data->get(4).asFloat64();
+      left_transform.header.stamp = right_transform.header.stamp = this->clock_->now();
+      left_transform.header.frame_id = right_transform.header.frame_id = "geometric_unicycle";
+      left_transform.child_frame_id = human_left_frame_;
+      right_transform.child_frame_id = human_right_frame_;
+      left_transform.transform.translation.x = human_left_from_robot(0, 3);
+      left_transform.transform.translation.y = human_left_from_robot(1, 3);
+      left_transform.transform.translation.z = human_left_from_robot(2, 3);
+      right_transform.transform.translation.x = human_right_from_robot(0, 3);
+      right_transform.transform.translation.y = human_right_from_robot(1, 3);
+      right_transform.transform.translation.z = human_right_from_robot(2, 3);
+      left_transform.transform.rotation.x = human_left_from_robot_quat.x();
+      left_transform.transform.rotation.y = human_left_from_robot_quat.y();
+      left_transform.transform.rotation.z = human_left_from_robot_quat.z();
+      left_transform.transform.rotation.w = human_left_from_robot_quat.w();
+
+      right_transform.transform.rotation.x = human_right_from_robot_quat.x();
+      right_transform.transform.rotation.y = human_right_from_robot_quat.y();
+      right_transform.transform.rotation.z = human_right_from_robot_quat.z();
+      right_transform.transform.rotation.w = human_right_from_robot_quat.w();
+
+      tf_broadcaster_->sendTransform(left_transform);
+      tf_broadcaster_->sendTransform(right_transform);
 
       RCLCPP_INFO(
           logger_,
