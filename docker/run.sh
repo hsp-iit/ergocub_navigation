85443fab83a021b097c0cbfaafa19c69ea7a9b2f
SimoneMic <simone_micheletti@outlook.it>
2026-05-20 15:16:23 +0000

    updated dockerfiles

--- docker/run.sh
+++ docker/run.sh
@@ -9,7 +9,14 @@ sudo docker run \
      --rm \
      -e DISPLAY=unix${DISPLAY} \
      -e ROS_DOMAIN_ID=11 \
+     --group-add video \
+     --group-add 110 \
      --device /dev/dri/card0:/dev/dri/card0 \
+     -e DISPLAY \
+     -v /tmp/.X11-unix:/tmp/.X11-unix \
+     -e QT_X11_NO_MITSHM=1 \
+     -e NVIDIA_DRIVER_CAPABILITIES=all \
+     -e __NV_PRIME_RENDER_OFFLOAD=1 \
+     -e __GLX_VENDOR_LIBRARY_NAME=nvidia \
+     -e __VK_LAYER_NV_optimus=NVIDIA_only \
      ${NAME}:${TAG} bash
-
-     #-v /home/simomic/rosbag2_2024_01_11-16_35_16:/home/ecub_docker/rosbags \
