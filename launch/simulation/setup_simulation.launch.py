d95d3c13888aea0cc1f9f8b1d8e1c6f38e52a1cb
SimoneMic <simone_micheletti@outlook.it>
2026-05-22 15:14:13 +0000

    simulation startup automation

--- launch/simulation/setup_simulation.launch.py
+++ launch/simulation/setup_simulation.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch.actions import ExecuteProcess, RegisterEventHandler, TimerAction, SetEnvironmentVariable
 from launch_ros.actions import Node
+from launch.event_handlers import OnProcessExit
 
 def generate_launch_description():
     set_yarp_clock = SetEnvironmentVariable(
@@ -11,7 +12,7 @@ def generate_launch_description():
     gazebo = ExecuteProcess(
         cmd=[
             "gz", "sim",
-            "/home/ecub_docker/ros2_workspace/src/ergocub_navigation/sim/warehouse.sdf",
+            "/home/ecub_docker/ros2_workspace/src/ergocub_navigation/sim/ionic.sdf",
             "--verbose",
             "-r"
         ],
@@ -41,13 +42,66 @@ def generate_launch_description():
               -file /home/ecub_docker/robotology-superbuild/src/ergocub-software/urdf/ergoCub/robots/ergoCubGazeboSN001_minContacts/model.urdf \
               -name ergocub \
               -z 0.8
-
-            sleep 1.5
-
-            yarprobotinterface --config /home/ecub_docker/robotology-superbuild/src/ergocub-software/urdf/ergoCub/conf/launch_wholebodydynamics_ecub.xml
             """
         ],
         output="screen",
+    )
+
+    wholebodydynamics = ExecuteProcess(
+        cmd=[
+            "yarprobotinterface",
+            "--config",
+            "/home/ecub_docker/robotology-superbuild/src/ergocub-software/urdf/ergoCub/conf/launch_wholebodydynamics_ecub.xml"
+        ],
+        output="screen",
+    )
+
+    head_imu = ExecuteProcess(
+        cmd=[
+            "yarprobotinterface",
+            "--config",
+            "/home/ecub_docker/ros2_workspace/src/ergocub_navigation/config/yarp/simulation/head_imu_ros2.xml"
+        ],
+        output="screen",
+    )
+
+    depth_compressed_ros2 = ExecuteProcess(
+        cmd=[
+            "yarprobotinterface",
+            "--config",
+            "/home/ecub_docker/ros2_workspace/src/ergocub_navigation/config/yarp/simulation/depth_compressed_ros2.xml"
+        ],
+        output="screen",
+    )
+
+    lidar_compressed_ros2 = ExecuteProcess(
+        cmd=[
+            "yarprobotinterface",
+            "--config",
+            "/home/ecub_docker/ros2_workspace/src/ergocub_navigation/config/yarp/simulation/lidar_compressed_ros2.xml"
+        ],
+        output="screen",
+    )
+
+    joint_states_republisher = ExecuteProcess(
+        cmd=[
+            "python3",
+            "/home/ecub_docker/ros2_workspace/src/ergocub_navigation/src/sim/joint_states_republisher.py",
+        ],
+        output="screen",
+    )
+
+    start_modules_after_spawn = RegisterEventHandler(
+        OnProcessExit(
+            target_action=spawn_robot_after_clock,
+            on_exit=[
+                TimerAction(period=1.5, actions=[wholebodydynamics]),
+                TimerAction(period=2.5, actions=[head_imu]),
+                TimerAction(period=3.5, actions=[depth_compressed_ros2]),
+                TimerAction(period=4.5, actions=[lidar_compressed_ros2]),
+                TimerAction(period=5.5, actions=[joint_states_republisher]),
+            ],
+        )
     )
 
     return LaunchDescription([
@@ -55,4 +109,5 @@ def generate_launch_description():
         gazebo,
         clock_bridge,
         spawn_robot_after_clock,
+        start_modules_after_spawn
     ])
