0d69b6513585de16506958796b2195636d424ae4
SimoneMic <simone_micheletti@outlook.it>
2026-06-04 10:35:06 +0000

    simulation gz sim update

--- launch/simulation/launch_sim.launch.py
+++ launch/simulation/launch_sim.launch.py
@@ -1,7 +1,7 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
+from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -24,6 +24,11 @@ def generate_launch_description():
         )
 
     return LaunchDescription([
+        Node(
+            package='ergocub_navigation',
+            executable='planner_trigger_server',
+            output='screen'
+            ),
         setup,
         localization,
         navigation
--- launch/simulation/launch_slam_sim.launch.py
+++ launch/simulation/launch_slam_sim.launch.py
@@ -1,7 +1,7 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
+from launch_ros.actions import Node
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -22,9 +22,20 @@ def generate_launch_description():
             get_package_share_directory('ergocub_navigation'), 'launch'),
             '/slam/slam_online_async.launch.py'])
         )
+    path_converter = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('ergocub_navigation'), 'launch'),
+            'path_converter.launch.py'])
+        )
 
     return LaunchDescription([
+        Node(
+            package='ergocub_navigation',
+            executable='planner_trigger_server',
+            output='screen'
+            ),
         setup,
         slam,
-        navigation
+        navigation,
+        #path_converter
     ])
